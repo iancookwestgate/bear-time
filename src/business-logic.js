116d87f60b8a4246fbc1379a98f279ba65bdb60d
Student <example@example.com>
2019-03-18 11:02:42 -0700

    fleshed out logic.

--- src/business-logic.js
+++ src/business-logic.js
@@ -1,7 +1,27 @@
-export class newClass {
-
-  constructor(classy) {
-    this.classy= classy;
+export let bear = {
+  foodLevel: 10,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => {
+      this.foodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(hungerInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
+  didYouGetEaten: function() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  feed: function(amount) {
+    let that = this;
+    return function(food) {
+      that.foodLevel += amount
+      return `The bear ate the ${food}! Food level goes up ${amount}!`
+    }
   }
-
-}
+};
+bear.eatSmall = bear.feed(5);
